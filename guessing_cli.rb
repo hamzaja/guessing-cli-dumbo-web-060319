ffb374cc058edcdc859c8058cfa3cd8f0f8c081c
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-31 03:06:24 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,12 @@
 def run_guessing_game
   p "You guessed the correct number!"
-  guess = gets.chomp.to_i
+  loop do 
+  guess = gets.chomp
   num = rand(1..100)
   if 
-    guess == num
-    p ""
-
+    guess.to_i == num
+    p "You guessed the correct number!"
+  break if num =="exit"
+end
 
 end
